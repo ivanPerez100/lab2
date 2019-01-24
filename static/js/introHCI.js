752a6eada784490c6fcb67f80a3598af7af7218b
igp002@ucsd.edu <ivanperez@Ivans-MacBook-Pro.local>
2019-01-24 10:51:31 -0800

    LAB 3 material

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,7 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("The Button Works!");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
 	});
 
 	// Add any additional listeners here
@@ -20,7 +22,20 @@ function initializePage() {
 }
 
 function projectClick(e){
-	
+	console.log("Project Clicked");
 	e.preventDefault();
 	$(this).css("background-color","#7fff00");
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else if( description.length == 1){
+    	console.log(description.length)
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       description.fadeOut(7000);
+    }
+
+
+
 }
